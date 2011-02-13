7a5f5335e05b5087479681247fcbad84b66135b3
Jeppe Toustrup <tenzer@tenzer.dk>
2011-02-13 22:58:30 +0100

    Added changes from commit 79bff68ac149f6ac9ba3853203efd9c64a664fbe to xbmc tree

--- xbmc.py
+++ xbmc.py
@@ -331,6 +331,10 @@ class InfoTagMusic(object):
         """Returns a string."""
         return str
 
+    def getAlbumArtist(self):
+        """Returns a string."""
+        return str
+
     def getAlbum(self):
         """Returns a string."""
         return str
@@ -351,7 +355,31 @@ class InfoTagMusic(object):
         """Returns an integer."""
         return int
 
+    def getTrackAndDisc(self):
+        """Returns an integer."""
+        return int
+
     def getReleaseDate(self):
+        """Returns a string."""
+        return str
+
+    def getListeners(self):
+        """Returns an integer."""
+        return int
+
+    def getPlayCount(self):
+        """Returns an integer."""
+        return int
+
+    def getLastPlayed(self):
+        """Returns a string."""
+        return str
+
+    def getComment(self):
+        """Returns a string."""
+        return str
+
+    def getLyrics(self):
         """Returns a string."""
         return str
 
@@ -416,6 +444,14 @@ class InfoTagVideo(object):
     def getRating(self):
         """Returns a float."""
         return float
+
+    def getPlayCount(self):
+        """Returns an integer."""
+        return int
+
+    def getLastPlayed(self):
+        """Returns a string."""
+        return str
 
 
 PLAYLIST_MUSIC = None
